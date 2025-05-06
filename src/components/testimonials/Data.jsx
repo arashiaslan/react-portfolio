ac4c6203bcf1eb35f23d6b83f10ac8d630113238
Imad Aqil <imaqil.mj@gmail.com>
2025-05-06 22:22:20 +0700

    update porto

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,6 +1,6 @@
-import Image1 from "../../assets/testimonial2.png";
-import Image2 from "../../assets/testimonial1.jpg";
-import Image3 from "../../assets/testimonial3.jpg";
+import Image1 from "../../assets/testimonial/testimonial2.png";
+import Image2 from "../../assets/testimonial/testimonial1.jpg";
+import Image3 from "../../assets/testimonial/testimonial3.jpg";
 
 export const Data = [
     {
--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,30 +1,76 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
-// import Work4 from "../../assets/work4.jpg";
-// import Work5 from "../../assets/work5.jpg";
+import Work1 from "../../assets/work/work1.jpg";
+import Aoc from "../../assets/work/cryptarithm.png";
+import Lingkungan from "../../assets/work/lingkungan.png";
+import Rebah from "../../assets/work/rebah.png";
+import Madnote from "../../assets/work/madnote.png";
+import Kopikita from "../../assets/work/kopikita.png";
+import Kalkulafiq from "../../assets/work/kalkulafiq.png";
+import Bdashboard from "../../assets/work/b-medix-dashboard.png";
+import Bukber from "../../assets/work/bukber.png";
 
 export const projects = [
     {
         id: 1,
         image: Work1,
         title: "Portal Warga: PerumKita (Laravel)",
-        demo: 'https://aesh.my.id/',
+        demo: 'https://github.com/arashiaslan/perumkita-laravel',
         category: "Web"
     },
     {
         id: 2,
-        image: Work2,
+        image: Aoc,
         title: "AOC-Math Game (Scracth)",
         demo: "https://arashiaslan.github.io/adventures-of-cryptarithm/",
         category: "App"
     },
     {
         id: 3,
-        image: Work3,
+        image: Lingkungan,
         title: "Poster Isu Lingkungan",
         demo: "https://www.canva.com/design/DAGEO3uCl9g/lbkX3BlzNPWlGQyejWgodA/edit?utm_content=DAGEO3uCl9g&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
         category: "Design"
+    },
+    {
+        id: 4,
+        image: Kopikita,
+        title: "Kopi Kita",
+        demo: "https://aesh-kopikita.vercel.app/",
+        category: "Web"
+    },
+    {
+        id: 5,
+        image: Madnote,
+        title: "Mad Note App",
+        demo: "https://madnote.vercel.app/",
+        category: "App"
+    },
+    {
+        id: 6,
+        image: Rebah,
+        title: "Rebah - Pomodoro App",
+        demo: "https://aesh-rebah.vercel.app/",
+        category: "App"
+    },
+    {
+        id: 7,
+        image: Kalkulafiq,
+        title: "FiqihKulator",
+        demo: "https://aesh-fikihkulator.vercel.app/",
+        category: "App"
+    },
+    {
+        id: 8,
+        image: Bukber,
+        title: "Poster Paket Bukber",
+        demo: "https://www.canva.com/design/DAGgQ4wF88Q/pKSTHKP6LXvw2sOoLrUV7A/edit?utm_content=DAGgQ4wF88Q&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+        category: "Design"
+    },
+    {
+        id: 9,
+        image: Bdashboard,
+        title: "B-Medix Admin Dashboard",
+        demo: "https://arashiaslan.github.io/b-medix-dashboard/",
+        category: "Web"
     },
 ];
 
